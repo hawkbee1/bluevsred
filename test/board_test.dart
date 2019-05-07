6b77455497e4e571fa2312ca4f419960136e51bf
Unknown <rbarbe@adsequor.fr>
2019-05-07 15:59:50 +0200

    [green] draggable widget exist

--- test/board_test.dart
+++ test/board_test.dart
@@ -11,20 +11,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bluevsred/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('draggable widget is working fine', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//    verify a draggable widget exist
+    expect(find.byType(Draggable), findsOneWidget);
+
+
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
   });
 }
