d44926b0adbd7b0a78ded30062a720c0f8663569
Unknown <rbarbe@adsequor.fr>
2019-05-07 10:58:58 +0200

    dragabble simple container

--- lib/main.dart
+++ lib/main.dart
@@ -45,19 +45,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  List<List<String>> gridState = [
-
-  ['', 'T', '', '', '', '', '', 'P2','', 'T', '', '', '', '', '', 'P2'],
-  ['', '', '', 'T', '', '', '', '','', 'T', '', '', '', '', '', 'P2'],
-  ['B', 'T', '', '', '', 'B', '', '','', 'T', '', '', '', '', '', 'P2'],
-  ['', '', '', 'B', '', '', '', 'T','', 'T', '', '', '', '', '', 'P2'],
-  ['', '', 'T', '', '', 'T', '', '','', 'T', '', '', '', '', '', 'P2'],
-  ['', '', '', '', '', '', '', 'B','', 'T', '', '', '', '', '', 'P2'],
-  ['', '', '', '', 'T', '', '', '','', 'T', '', '', '', '', '', 'P2'],
-  ['P1', '', '', '', '', '', 'T', '','', 'T', '', '', '', '', '', 'P2'],
-  ['P1', '', '', '', '', '', 'T', '','', 'T', '', '', '', '', '', 'P2'],
-
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -73,18 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          DraggableWidget(offset: Offset(50.0,100.0),),
-          Container(
-            decoration: BoxDecoration(color: Colors.lightGreenAccent),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('HRER'),
-          ),),
-          _buildGameBody(),
-        ],
-      ),
+      body: DraggableWidget(offset: Offset(50.0,100.0),),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
         tooltip: 'Increment',
@@ -93,88 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildGameBody() {
-    int gridStateLength = gridState.length;
-    return Column(
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1.0,
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.green, width: 2.0)
-              ),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 8,
 
-                ),
-                itemBuilder: _buildGridItems,
-                itemCount: gridStateLength * gridStateLength,
-              ),
-            ),
-          ),
-        ]);
-  }
-  Widget _buildGridItems(BuildContext context, int index) {
-    int gridStateLength = gridState.length;
-    int x, y = 0;
-    x = (index / gridStateLength).floor();
-    y = (index % gridStateLength);
-    return GestureDetector(
-      onTap: () => _gridItemTapped(x, y),
-      child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 0.5)
-          ),
-          child: Center(
-            child: _buildGridItem(x, y),
-          ),
-        ),
-      ),
-    );
-  }
-  Widget _buildGridItem(int x, int y) {
-    switch (gridState[x][y]) {
-      case '':
-        return Text('');
-        break;
-
-      case 'P1':
-        return Container(
-          color: Colors.blue,
-        );
-        break;
-
-      case 'P2':
-        return Container(
-          color: Colors.yellow,
-        );
-        break;
-
-      case 'T':
-        return Icon(
-          Icons.terrain,
-          size: 40.0,
-          color: Colors.red,
-        );
-        break;
-
-      case 'B':
-        return Icon(Icons.remove_red_eye, size: 40.0);
-        break;
-
-      default:
-        return Text(gridState[x][y].toString());
-    }
-  }
-
-}
-
-_gridItemTapped(int x, int y) {
-  print('OK');
 }
 
 class DraggableWidget extends StatefulWidget {
@@ -197,21 +92,37 @@ class _StateDraggableWidget extends State<DraggableWidget> {
     return Stack(
       children: <Widget>[
         Positioned(
+          bottom: 100.0,
+          right: 100.0,
+          child: Container(
+            child: Center(child:Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('x: ${widget.offset.dx.floor()} y: ${widget.offset.dy.floor()}'),
+            )),
+            decoration: BoxDecoration(color: Colors.red),
+          ),
+        ),
+        Positioned(
         top: widget.offset.dy,
         left: widget.offset.dx,
         child: Draggable(
           feedback: Container(
             child: Center(child:Text('good')),
-            height: 20.0,
-            width: 20.0,
+            height: 82.0,
+            width: 120.0,
             decoration: BoxDecoration(color: Colors.red),
           ),
           child: Container(
             child: Center(child:Text('good')),
-            height: 20.0,
-              width: 20.0,
+            height: 150.0,
+              width: 120.0,
             decoration: BoxDecoration(color: Colors.green),
           ),
+          onDraggableCanceled: (v,o) {
+            setState(() {
+              widget.offset = o;
+            });
+          },
         ),
       ),
       ],
