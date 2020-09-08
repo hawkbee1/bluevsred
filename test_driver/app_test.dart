620a4762530d0682c745a87a97ff897efe8aba64
hawkbee <googandads@gmail.com>
2020-09-08 23:22:40 +0200

    install flutter_driver properly

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,25 +1,23 @@
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:screenshots/screenshots.dart';
+import 'package:test/test.dart';
 
 void main() {
   final config = Config();
   FlutterDriver driver;
   final textFinder = find.byValueKey('first-unit');
-group('Test GDPR', () {
-   
+  group('Test GDPR', () {
     setUpAll(
       () async {
         driver = await FlutterDriver.connect();
       },
     );
-test(
-      'Tap the screen',
-      () async {
+    test('Tap the screen', () async {
 //        await driver.tap(buttonFinder);
 //        expect(await driver.getText(textFinder), "1");
-        await screenshot(driver, config, 'testing');
-      }),
-    );
-tearDownAll(() {
+      await screenshot(driver, config, 'testing');
+    });
+    tearDownAll(() {
       if (driver != null) {
         driver.close();
       }
