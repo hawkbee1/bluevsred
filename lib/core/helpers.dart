4e04a3baedfc26e0b2e0cbd6ed274626bbff34c3
hawkbee1 <googandads@gmail.com>
2020-10-06 09:31:24 +0200

    Add screen reader test for gdpr

--- lib/core/helpers.dart
+++ lib/core/helpers.dart
@@ -2,7 +2,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 //TODO: Should I write some tests for this ?
 /// A function which open provided url in a browser if possible
-/// If url can not be launched, launchURL throw an error
+/// If url can not be launched, [launchURL] throw an error
 launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
