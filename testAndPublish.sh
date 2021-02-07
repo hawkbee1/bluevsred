9c2f0ff98f71f39ef18d7d6eb9b3e94c17f55f0f
hawkbee1 <googandads@gmail.com>
2021-02-07 12:59:13 +0100

    Add parts of roddy davis deployment script

--- testAndPublish.sh
+++ testAndPublish.sh
@@ -1,9 +1,36 @@
 #sh
 
-echo "lancement script"
-flutter clean
-flutter build appbundle
-flutter build ios
+echo "Adaptation of App Release Automator by @rodydavis"
+
+action="$1"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+   echo "${green}Generating built files.. ${reset}"
+    flutter packages pub run build_runner clean
+    flutter packages pub run build_runner build --delete-conflicting-outputs
+
+    pub global activate pubspec_version
+#    git commit -a -m "Build $(pubver bump patch)"
+
+    echo "${green}Building Project...${reset}"
+    find . -name "*-e" -type f -delete
+    flutter format .
+    flutter clean
+
+    echo "${green}Project Size: $(find . -name "*.dart" | xargs cat | wc -c)${reset}"
+
+    echo "${green}Building appbundle...${reset}"
+    flutter build appbundle
+
+    echo "${green}Builing IPA..${reset}"
+    cd ./ios && pod install && pod repo update && cd ..
+    flutter build ios
+
+#    git commit -a -m "Project Rebuilt"
+
+
 
 cd android
 fastlane test
