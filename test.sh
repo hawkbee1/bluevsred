147e4cfc62e96526040e60e4b531fd6b2356cc63
hawkbee1 <googandads@gmail.com>
2021-03-01 19:30:34 +0100

    Add script specific to tests

---
+++ test.sh
@@ -0,0 +1,41 @@
+#sh
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>logTests
+
+# Redirect STDERR to STDOUT
+exec 2>&1
+
+echo "Adaptation of App Release Automator by @rodydavis"
+
+action="$1"
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+git fetch
+commitIdOrigin=$(git log --format="%H" -n 1 --remotes=origin)
+commitIdLocal=$(git log --format="%H" -n 1)
+
+if [ $commitIdOrigin != $commitIdLocal ]
+then
+   git pull
+   echo "${green}Preparing environment ${reset}"
+   flutter clean
+   flutter pub get
+   flutter packages pub run build_runner clean
+   echo "${green}Unit tests${reset}"
+    flutter test
+   echo "${green}Android driver tests${reset}"
+    flutter drive --target=test_driver/main.dart -d emulator-5554
+   echo "${green}Ios driver tests${reset}"
+    flutter drive --target=test_driver/main.dart -d 0CAF54DC-CA9C-4AFB-85BA-6392DF3F0C80
+    pwd
+fi
+echo "process finished"
+
+
