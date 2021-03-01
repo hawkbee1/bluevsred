9ca9ef24c0b80b0b022e8608858903aee908be57
hawkbee1 <googandads@gmail.com>
2021-03-01 16:37:15 +0100

    Add commit check and normal test_drive command to launc tests

--- testAndPublish.sh
+++ testAndPublish.sh
@@ -7,6 +7,12 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
+commitIdOrigin=git log --format="%H" -n 1 --remotes=origin
+commitIdLocal=git log --format="%H" -n 1
+
+if [ commitIdOrigin != commitIdLocal]
+then
+   git pull
    echo "${green}Generating built files.. ${reset}"
     flutter packages pub run build_runner clean
     flutter packages pub run build_runner build --delete-conflicting-outputs
@@ -26,13 +32,16 @@ reset=`tput sgr0`
 
     echo "${green}Builing IPA..${reset}"
     cd ./ios && pod deintegrate && pod install && pod update
+    fastlane test
+    flutter drive --target=test_driver/main.dart
     cd ..
     flutter build ios
+    pwd
 
+    cd android
+    fastlane test
+fi
+echo "process finished"
 #    git commit -a -m "Project Rebuilt"
 
 
-pwd
-
-cd android
-fastlane test
