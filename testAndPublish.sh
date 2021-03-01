409c9226cab5445d263282d721c572cad02001cd
hawkbee1 <googandads@gmail.com>
2021-03-01 17:36:38 +0100

    Redirect echo and script result from testAndroid to log file

--- testAndPublish.sh
+++ testAndPublish.sh
@@ -1,4 +1,14 @@
 #sh
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>logFile
+
+# Redirect STDERR to STDOUT
+exec 2>&1
 
 echo "Adaptation of App Release Automator by @rodydavis"
 
