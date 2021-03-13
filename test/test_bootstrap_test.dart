95b5f3a8b88ede3d8f79c54ad9994c5bbc9c3eeb
hawkbee1 <googandads@gmail.com>
2021-03-13 11:31:28 +0100

    [GREEN] Can unit do action ?

--- test/test_bootstrap_test.dart
+++ test/test_bootstrap_test.dart
@@ -3,7 +3,7 @@ import 'package:bluevsred/entities/game_unit.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Throw an error if acting unit does not have enough action points', () {
+  group('Can unit do action ?', () {
     final GameUnit activeUnit = GameUnit();
     final GameAction gameAction = GameAction(activeUnit: activeUnit);
 
@@ -14,30 +14,34 @@ void main() {
 
     test('canActionHappen return Right(true) when Unit has enough points', () {
       gameAction.actionPointsCost = 5;
-      var response = gameAction.canActionHappen();
-      bool right;
-      response.fold((l) => null, (r) => right = r);
-      expect(right, isTrue);
+      checkCanActionHappen(gameAction, false, true);
     });
 
     test('canActionHappen return Right(true) when Unit has exactly the points',
         () {
       gameAction.actionPointsCost = 10;
-      var response = gameAction.canActionHappen();
-      bool right;
-      response.fold((l) => null, (r) => right = r);
-      expect(right, isTrue);
+      checkCanActionHappen(gameAction, false, true);
     });
 
-    test('canActionHappen return Right(Error) when Unit has not enough points',
+    test('canActionHappen return Left(Error) when Unit has not enough points',
         () {
       gameAction.actionPointsCost = 20;
-      var response = gameAction.canActionHappen();
-      var left;
-      bool isLeftAnError = false;
-      response.fold((l) => left = l, (r) => null);
-      left is Error ? isLeftAnError = true : isLeftAnError = false;
-      expect(isLeftAnError, isTrue);
+      checkCanActionHappen(gameAction, true, null);
     });
   });
+}
+
+void checkCanActionHappen(
+  GameAction gameAction,
+  bool isLeftAnErrorExpectation,
+  bool rightExpectation,
+) {
+  var response = gameAction.canActionHappen();
+  var left;
+  var right;
+  bool isLeftAnError = false;
+  response.fold((l) => left = l, (r) => right = r);
+  left is Error ? isLeftAnError = true : isLeftAnError = false;
+  expect(isLeftAnError, isLeftAnErrorExpectation);
+  expect(right, rightExpectation);
 }
