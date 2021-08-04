b49673c4717fbbd3956b135094923bdff9fa55fa
hawkbee1 <googandads@gmail.com>
2021-08-04 21:25:20 +0200

    before major upgrade

--- test_driver/tests/gdpr_test.dart
+++ test_driver/tests/gdpr_test.dart
@@ -22,19 +22,20 @@ void main() {
     final acceptGdprSettingsButtonFinder = find.text('ACCEPT');
     test('All the GDPR process on sign up which end by displaying the Board',
         () async {
-      takeScreenshot(driver, 'gdpr_test_homePage');
+      print('suppose to ');
+      takeScreenshot(driver, 'gdpr_test_homePage.png');
       await driver.tap(signUpButtonFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprLandingPage');
+      takeScreenshot(driver, 'gdpr_test_gdprLandingPage.png');
       await driver.tap(manageGdprSettingsButtonFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprSettingsPage');
+      takeScreenshot(driver, 'gdpr_test_gdprSettingsPage.png');
       await driver.tap(manageAnalyticsSwitchFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprAnalytics_false');
+      takeScreenshot(driver, 'gdpr_test_gdprAnalytics_false.png');
       await driver.tap(manageAdvertisingSwitchFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprAdvertising_false');
+      takeScreenshot(driver, 'gdpr_test_gdprAdvertising_false.png');
       await driver.tap(manageAnalyticsSwitchFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprAnalytics_true');
+      takeScreenshot(driver, 'gdpr_test_gdprAnalytics_true.png');
       await driver.tap(acceptGdprSettingsButtonFinder);
-      takeScreenshot(driver, 'gdpr_test_gdprBoardPage');
+      takeScreenshot(driver, 'gdpr_test_gdprBoardPage.png');
     });
     tearDownAll(() {
       if (driver != null) {
