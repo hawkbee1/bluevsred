4d86620e012e34802a195b1687afb31f1f9120b5
hawkbee1 <googandads@gmail.com>
2021-10-11 18:29:23 +0200

    Erase previous code

--- android/app/src/main/kotlin/fr/whereassistant/bluevsred/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package fr.whereassistant.bluevsred
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- integration_test/main_test.dart
+++
@@ -1,5 +0,0 @@
-import 'tests/gdpr_test.dart' as gdpr_test;
-
-void main() {
-  gdpr_test.main();
-}
