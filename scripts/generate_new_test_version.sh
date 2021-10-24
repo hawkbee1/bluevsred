f0e70d38175ab2cffa70fbcfcff6d1c0c4fe05e1
hawkbee1 <googandads@gmail.com>
2021-10-24 15:31:49 +0200

    Changed way od specifing aab path

--- scripts/generate_new_test_version.sh
+++ scripts/generate_new_test_version.sh
@@ -7,7 +7,7 @@ flutter test --coverage --test-randomize-ordering-seed random
 genhtml coverage/lcov.info -o coverage/
 flutter build appbundle --flavor development --target lib/main_development.dart
 cd android
-bundle exec fastlane deploy
+bundle exec fastlane deploy --aab ../build/app/outputs/bundle/release/app-release.aab
 cd ..
 flutter build ios --flavor development --target lib/main_development.dart
 cd ios
