1cf5f15acd8dd79b3c9ea7252bad7a93bc245523
hawkbee1 <googandads@gmail.com>
2021-10-24 19:41:15 +0200

    Set correct path to appbundle

--- scripts/generate_new_test_version.sh
+++ scripts/generate_new_test_version.sh
@@ -7,9 +7,9 @@ flutter test --coverage --test-randomize-ordering-seed random
 genhtml coverage/lcov.info -o coverage/
 flutter build appbundle --flavor development --target lib/main_development.dart
 cd android
-fastlane supply --aab ../build/app/outputs/bundle/developmentRelease/app-development-release.aab --track internal
+fastlane supply --aab /Users/romualdbarbe/Documents/bluevsred/build/app/outputs/bundle/developmentRelease/app-development-release.aab --track internal
 bundle exec fastlane deploy
 cd ..
-flutter build ios --flavor development --target lib/main_development.dart
-cd ios
-bundle exec fastlane beta
+#flutter build ios --flavor development --target lib/main_development.dart
+#cd ios
+#bundle exec fastlane beta
