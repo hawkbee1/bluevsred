ba16dca63856d7098593b8a8a624281d1cf2db86
hawkbee1 <googandads@gmail.com>
2021-10-25 20:29:33 +0200

    Activate android publication script

--- scripts/generate_new_test_version.sh
+++ scripts/generate_new_test_version.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
 cd ..
-#flutter clean
-#flutter pub get
-#flutter test --coverage --test-randomize-ordering-seed random
-#genhtml coverage/lcov.info -o coverage/
-#flutter build appbundle --flavor development --target lib/main_development.dart
+flutter clean
+flutter pub get
+flutter test --coverage --test-randomize-ordering-seed random
+genhtml coverage/lcov.info -o coverage/
+flutter build appbundle --flavor development --target lib/main_development.dart
 cd android
-fastlane supply --aab /Users/romualdbarbe/Documents/bluevsred/build/app/outputs/bundle/developmentRelease/app-development-release.aab --track internal --skip_upload_apk  true --skip_upload_metadata true
-#skip_upload_changelogs          | false                                                            |
-#| skip_upload_images              | false                                                            |
-#| skip_upload_screenshots         | false
+fastlane supply --aab /Users/romualdbarbe/Documents/bluevsred/build/app/outputs/bundle/developmentRelease/app-development-release.aab --track internal
 bundle exec fastlane deploy
 cd ..
 #flutter build ios --flavor development --target lib/main_development.dart
