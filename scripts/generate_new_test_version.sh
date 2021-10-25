2d8bd798b4dc6e62bf8c9156a2fa6731060b5570
hawkbee1 <googandads@gmail.com>
2021-10-25 20:57:32 +0200

    CI/CD bug correction

--- scripts/generate_new_test_version.sh
+++ scripts/generate_new_test_version.sh
@@ -9,7 +9,7 @@ genhtml coverage/lcov.info -o coverage/
 flutter build appbundle --flavor development --target lib/main_development.dart
 cd android
 fastlane supply --aab /Users/romualdbarbe/Documents/bluevsred/build/app/outputs/bundle/developmentRelease/app-development-release.aab --track internal
-bundle exec fastlane upload_to_play_store
+bundle exec fastlane deploy
 cd ..
 #flutter build ios --flavor development --target lib/main_development.dart
 #cd ios
