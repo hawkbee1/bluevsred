33fef107c45303e6edd69d4a5fd60ef18cf8fae4
hawkbee1 <googandads@gmail.com>
2021-10-26 22:25:12 +0200

    Add first integration test

--- scripts/test_last_commit.sh
+++ scripts/test_last_commit.sh
@@ -6,5 +6,9 @@ flutter clean
 flutter pub get
 flutter test --coverage --test-randomize-ordering-seed random
 genhtml coverage/lcov.info -o coverage/
+flutter emulator --launch Pixel_4_API_29_with_google_play
+flutter test --flavor development integration_test
+flutter emulator --launch Pixel_4_API_29_with_google_play
+flutter test --flavor development integration_test
 flutter build appbundle --flavor development --target lib/main_development.dart
 flutter build ios --flavor development --target lib/main_development.dart
