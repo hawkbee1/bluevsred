37b7a6c28dfe655773bc3871131ae7777e34cd3e
hawkbee1 <googandads@gmail.com>
2021-10-27 17:48:43 +0200

    improve dev testing with devices on M1

--- scripts/test_last_commit.sh
+++ scripts/test_last_commit.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-
+#fvm flutter emulator --launch Pixel_4_API_31_arl64_M1
+#fvm flutter emulator --launch apple_ios_simulator
 cd ..
 git pull
-flutter clean
-flutter pub get
-flutter test --coverage --test-randomize-ordering-seed random
+fvm flutter clean
+fvm flutter pub get
+fvm flutter test --coverage --test-randomize-ordering-seed random
 genhtml coverage/lcov.info -o coverage/
-flutter emulator --launch Pixel_4_API_29_with_google_play
-flutter test --flavor development integration_test
-flutter emulator --launch Pixel_4_API_29_with_google_play
-flutter test --flavor development integration_test
-flutter build appbundle --flavor development --target lib/main_development.dart
-flutter build ios --flavor development --target lib/main_development.dart
+
+fvm flutter test --flavor development -d emulator-5554 integration_test
+
+fvm flutter test --flavor development -d 78BBB2B3-6DB9-4A7F-9D41-9DBF6D5B077D integration_test
+fvm flutter build appbundle --flavor development --target lib/main_development.dart
+fvm flutter build ios --flavor development --target lib/main_development.dart
