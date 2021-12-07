fb84d539fd7fc7a8a3bb371444df85053f999731
hawkbee1 <googandads@gmail.com>
2021-12-07 17:33:31 +0100

    linter

--- test/shared/app_text_field.dart
+++ test/shared/app_text_field.dart
@@ -4,13 +4,15 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('AppTextField', () {
-    testWidgets('key and Hint text are in the widget AppTextField', (tester) async {
+    testWidgets('key and Hint text are displayed', (tester) async {
       const appTextFieldKey = Key('test');
       const appTextFieldHintText = 'Hint text';
-      await tester.pumpWidget(const AppTextField(
-        appTextFieldKey: appTextFieldKey,
-        appTextFieldHintText: appTextFieldHintText,
-      ));
+      await tester.pumpWidget(
+        const AppTextField(
+          appTextFieldKey: appTextFieldKey,
+          appTextFieldHintText: appTextFieldHintText,
+        ),
+      );
       expect(find.byKey(appTextFieldKey), findsOneWidget);
       expect(find.byType(AppTextField), findsOneWidget);
       expect(find.text(appTextFieldHintText), findsOneWidget);
