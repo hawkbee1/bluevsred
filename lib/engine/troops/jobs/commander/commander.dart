42b9406169d641c40af739954f49184188426d2c
hawkbee1 <googandads@gmail.com>
2024-03-26 20:54:50 +0000

    backup :D

---
+++ lib/engine/troops/jobs/commander/commander.dart
@@ -0,0 +1,5 @@
+import 'package:endless_runner/engine/troops/troop.dart';
+
+class CommanderTroop extends Troop {
+ CommanderTroop.create({required super.gamePlayer});
+}
--- test/engine_test.dart
+++ test/engine_test.dart
@@ -39,6 +39,39 @@ void main() {
         resultingTroopList.remove((element) => element != troop);
         expect(resultingTroopList.length, equals(1));
       });
+      test(
+          'When adding GamePlayer\'s troops to a battle, at least one Commander Troop is added to a battle.',
+          () {
+        final gamePlayer = GamePlayer(name: 'adding commander');
+        final troop = CommanderTroop(gamePlayer: gamePlayer);
+        final battle = Battle();
+        battle.addTroops(newTroops: [troop, troop]);
+        final resultingTroopList = List.from(battle.troops);
+        resultingTroopList.remove((element) => element != troop);
+        expect(resultingTroopList.length, equals(1));
+      });
+      test(
+          'When adding troops, only one Commander of a GamePlayer GameTroops is added to a battle.',
+          () {
+        final gamePlayer = GamePlayer(name: 'test player name');
+        final troop = CommanderTroop(gamePlayer: gamePlayer);
+        final battle = Battle();
+        battle.addTroops(newTroops: [troop, troop]);
+        final resultingTroopList = List.from(battle.troops);
+        resultingTroopList.remove((element) => element != troop);
+        expect(resultingTroopList.length, equals(1));
+      });
+      test(
+          'When adding a Commander to a battle he must not be already part of another battle.',
+          () {
+        final gamePlayer = GamePlayer(name: 'test player name');
+        final troop = CommanderTroop(gamePlayer: gamePlayer);
+        final battle = Battle();
+        battle.addTroops(newTroops: [troop, troop]);
+        final resultingTroopList = List.from(battle.troops);
+        resultingTroopList.remove((element) => element != troop);
+        expect(resultingTroopList.length, equals(1));
+      });
       test('Team added to a battle is in the list of teams', () {
         final team = Team(
           name: 'test team blue',
@@ -93,15 +126,38 @@ void main() {
         );
         final gamePlayer = GamePlayer(name: 'test player');
         final battle = Battle();
-        battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer, gamePlayer], team: team);
+        battle.addGamePlayersToTeam(
+            newGamePlayers: [gamePlayer, gamePlayer], team: team);
         battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer], team: team);
-        
+
         final resultingGamePlayerList = [];
-         for( final element in battle.teams) {
+        for (final element in battle.teams) {
           resultingGamePlayerList.addAll(element.gamePlayers);
-         }
+        }
         resultingGamePlayerList.remove((element) => element != gamePlayer);
         expect(resultingGamePlayerList.length, equals(1));
+      });
+      test(
+          'When adding players in a team already in battle, ensure we don\'t loose old players',
+          () {
+        final team = Team(
+          name: 'test team blue',
+          colorCode: '0000FF',
+        );
+        final gamePlayer1 = GamePlayer(name: 'first test player');
+        final gamePlayer2 = GamePlayer(name: 'second test player');
+
+        final battle = Battle();
+        battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer1], team: team);
+
+        battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer2], team: team);
+
+        final resultingGamePlayerList = [];
+        for (final element in battle.teams) {
+          resultingGamePlayerList.addAll(element.gamePlayers);
+        }
+        expect(resultingGamePlayerList, contains(gamePlayer1));
+        expect(resultingGamePlayerList, contains(gamePlayer2));
       });
       test('A player can be added only once to a battle.', () {
         final teamBlue = Team(
@@ -115,8 +171,10 @@ void main() {
         final battle = Battle();
         battle.addTeams(newTeams: [teamRed, teamBlue]);
         final gamePlayer = GamePlayer(name: 'test player');
-        battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer], team: teamBlue);
-        battle.addGamePlayersToTeam(newGamePlayers: [gamePlayer], team: teamRed);
+        battle
+            .addGamePlayersToTeam(newGamePlayers: [gamePlayer], team: teamBlue);
+        battle
+            .addGamePlayersToTeam(newGamePlayers: [gamePlayer], team: teamRed);
         final resultingGamePlayerList = [];
         for (final list in battle.teams) {
           resultingGamePlayerList.addAll(list.gamePlayers);
@@ -149,4 +207,7 @@ void main() {
   });
   // This game let players engage their troops in battles. Battles are between red
   // and blue teams and end by destruction of a team Headquarters'
+}
+
+class CommanderTroop {
 }
