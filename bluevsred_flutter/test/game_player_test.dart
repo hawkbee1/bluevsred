e39f0aa3284267179e2b98b4d25b2dd3f39a7656
hawkbee1 <googandads@gmail.com>
2024-04-04 14:47:31 +0000

    codecov initial setup

--- bluevsred_flutter/test/game_player_test.dart
+++ bluevsred_flutter/test/game_player_test.dart
@@ -1,9 +1,9 @@
-import 'package:bluevsred_shared/src/game_player/game_player.dart';
+// import 'package:bluevsred_shared/src/game_player/game_player.dart';
 import 'package:test/test.dart';
 
 void main() {
-  const String gamePlayer1Name = 'first test player';
-  final List<GamePlayer> initialPlayerList = [GamePlayer(name: 'initial player 1'), GamePlayer(name: 'initial player 2'), GamePlayer(name: 'initial player 3'), GamePlayer(name: 'initial player 4'),];
+  // const String gamePlayer1Name = 'first test player';
+  // final List<GamePlayer> initialPlayerList = [GamePlayer(name: 'initial player 1'), GamePlayer(name: 'initial player 2'), GamePlayer(name: 'initial player 3'), GamePlayer(name: 'initial player 4'),];
   group('player lifecycle', () {
         test('when game player is created, he appear in the player list', () {
           // Here comes the need of state management system
@@ -16,14 +16,14 @@ void main() {
 
     } );
 
-    test('when game player is created, his first commander is created', () {
-      bool isPlayerCommanderCreated =  false;
-      final  gamePlayer = GamePlayer(name: gamePlayer1Name);
-      isPlayerCommanderCreated =  true;
+    // test('when game player is created, his first commander is created', () {
+    //   bool isPlayerCommanderCreated =  false;
+    //   final  gamePlayer = GamePlayer(name: gamePlayer1Name);
+    //   isPlayerCommanderCreated =  true;
 
-    } );
-    test('when game player is created, its name must be unique', () {
-      final  gamePlayer = GamePlayer(name: gamePlayer1Name); 
-    } );
+    // } );
+    // test('when game player is created, its name must be unique', () {
+    //   final  gamePlayer = GamePlayer(name: gamePlayer1Name); 
+    // } );
   });
 }
